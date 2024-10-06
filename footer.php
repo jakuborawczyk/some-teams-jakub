e62edc4dce30168c349b374a890c6a0ce0e19cb2
Jakub Orawczyk <jakuboraw@gmail.com>
2024-10-06 15:15:12 +0200

    made navbar and footer

---
+++ footer.php
@@ -0,0 +1,6 @@
+ <!-- Footer -->
+ <footer class="footer">
+        <div class="container">
+            <p>&copy; 2024 Jakub Orawczyk. All rights reserved.</p>
+        </div>
+    </footer>
---
+++ navbar.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>some-teams</title>
+</head>
+<body>
+
+<!-- Navbar -->
+<nav class="navbar">
+        <div class="container">
+            <a href="index.php" class="logo">Jakub Orawczyk</a>
+            <ul class="nav-links">
+                <li><a href="aboutMe.php">About</a></li>
+                <li><a href="#portfolio">Portfolio</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </div>
+    </nav>
+
+   
+   
+
+</body>
+</html>
