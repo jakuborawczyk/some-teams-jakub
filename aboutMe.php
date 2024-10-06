2f0fe5f7b137ec4692c9b3018d66230f9676cf82
Jakub Orawczyk <jakuboraw@gmail.com>
2024-10-06 15:17:51 +0200

    made about me page and broke the files up

---
+++ aboutMe.php
@@ -0,0 +1,20 @@
+<?php require __DIR__ ."/navbar.php"?>
+ 
+ <!-- Main Content -->
+ <section id="about" class="about-section">
+    <div class="container">
+        <div class="about-content">
+            <div class="about-photo">
+                <!-- IMG -->
+                <img src="FB_IMG_1715598716335.jpg" alt="Your Name" />
+            </div>
+            <div class="about-info">
+                <h1>About Me</h1>
+                <p>Hi, I’m <strong>Jakub Orawczyk</strong>, a 19-year-old passionate about web development. My journey into programming started in 2019 during an internship at Albert AB, where I discovered my love for creating digital solutions. After a year, I had the opportunity to work there as a QA tester during the summer, diving deeper into the world of programming.</p>
+                <p>Besides coding, I enjoy spending my free time with friends, exploring new things, and learning continuously. My mission is to create impactful web experiences that bring value to users.</p>
+            </div>
+        </div>
+    </div>
+</section>
+ 
+<?php require __DIR__ ."/footer.php" ?>
