92f0536631340adec2dd47f4e9ddb7401382bc64
Jakub Orawczyk <jakuboraw@gmail.com>
2024-10-09 09:21:55 +0200

    fixed responsivity for the start page

--- navbar.php
+++ navbar.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
+    <link rel="icon" href="Group 3.png" type="image/png" sizes="30x30">
     <title>some-teams</title>
 </head>
 <body>
